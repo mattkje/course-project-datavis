4b8abe02f7179ec7cf1dfda74da154308ee1196b
Matti Kjellstadli <40216910+mattkje@users.noreply.github.com>
2024-10-28 22:18:49 +0100

    Fix bug

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,6 @@
 <script setup>
 
 import Globe from "@/components/Globe.vue";
-<<<<<<< Updated upstream
-import SearchBar from "@/components/SearchBar.vue";
-</script>
-
-<template>
-  <main>
-   <SearchBar />
-
-  </main>
-=======
 import ToolBar from "@/components/ToolBar.vue";
 import CountryInfoBox from "@/components/CountryInfoBox.vue";
 </script>
@@ -19,7 +9,6 @@ import CountryInfoBox from "@/components/CountryInfoBox.vue";
 <ToolBar />
     <Globe />
   <CountryInfoBox />
->>>>>>> Stashed changes
 </template>
 
 <style scoped>
