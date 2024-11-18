9be963f1bec23139aad7ecc11c04949ae87e6d66
adfajo <adrfaujoh@hotmail.com>
2024-11-18 18:08:26 +0100

    added more API calls

--- backend/app.py
+++ backend/app.py
@@ -10,64 +10,66 @@ def filter_world_data():
     filtered_columns = df[["country", "Abbreviation", "Land Area(Km2)", "Capital/Major City", "Largest city"]]
     return filtered_columns
 
+def init_global_data():
+    df1 = pd.read_csv("datasets/globalwarmingdata.csv")
+    df2 = filter_world_data()
+    return pd.merge(df1, df2, on="country")
+
 @app.route("/")
 def home():
     return "Welcome to Group 5's API! :)"
 
 @app.route("/country/<country>")
 def country(country):
-    df1 = pd.read_csv("datasets/globalwarmingdata.csv")
-    df2 = filter_world_data()
-    merged_df = pd.merge(df1, df2, on="country")
-    result_df = merged_df[merged_df["country"] == country]
+    result_df = init_global_data()
+    result_df = result_df[result_df["country"] == country]
     result_df.fillna("N/A", inplace=True)  # Replace NaN values with "N/A"
     return jsonify(result_df.to_dict(orient="records"))
 
 @app.route("/year/<year>")
 def year(year):
-    df1 = pd.read_csv("datasets/globalwarmingdata.csv")
-    df2 = filter_world_data()
-    merged_df = pd.merge(df1, df2, on="country")
+    merged_df = init_global_data()
     result_df = merged_df[merged_df["year"] == int(year)]
     result_df.fillna("N/A", inplace=True)  # Replace NaN values with "N/A"
     return jsonify(result_df.to_dict(orient="records"))
 
 @app.route("/country_year/<country>/<year>")
 def country_year(country, year):
-    df1 = pd.read_csv("datasets/globalwarmingdata.csv")
-    df2 = filter_world_data()
-    merged_df = pd.merge(df1, df2, on="country")
+    merged_df = init_global_data()
     result_df = merged_df[(merged_df["country"] == country) & (merged_df["year"] == int(year))]
     result_df.fillna("N/A", inplace=True)  # Replace NaN values with "N/A"
     return jsonify(result_df.to_dict(orient="records"))
 
 @app.route("/country_year_range/<country>/<start_year>/<end_year>")
 def country_year_range(country, start_year, end_year):
-    df1 = pd.read_csv("datasets/globalwarmingdata.csv")
-    df2 = filter_world_data()
-    merged_df = pd.merge(df1, df2, on="country")
+    merged_df = init_global_data()
     result_df = merged_df[(merged_df["country"] == country) & (merged_df["year"] >= int(start_year)) & (merged_df["year"] <= int(end_year))]
     result_df.fillna("N/A", inplace=True)  # Replace NaN values with "N/A"
     return jsonify(result_df.to_dict(orient="records"))
 
 @app.route("/country_code/<country_code>")
 def country_code(country_code):
-    df1 = pd.read_csv("datasets/globalwarmingdata.csv")
-    df2 = filter_world_data()
-    merged_df = pd.merge(df1, df2, on="country")
+    merged_df = init_global_data()
     result_df = merged_df[merged_df["iso_code"] == country_code]
     result_df.fillna("N/A", inplace=True)  # Replace NaN values with "N/A"
     return jsonify(result_df.to_dict(orient="records"))
 
 @app.route("/country_code_latest/<country_code>")
 def country_code_latest(country_code):
-    df1 = pd.read_csv("datasets/globalwarmingdata.csv")
-    df2 = filter_world_data()
-    merged_df = pd.merge(df1, df2, on="country")
+    merged_df = init_global_data()
     result_df = merged_df[merged_df["iso_code"] == country_code]
     result_df = result_df[result_df["year"] == result_df["year"].max()]
     result_df.fillna("N/A", inplace=True)  # Replace NaN values with "N/A"
     return jsonify(result_df.to_dict(orient="records"))
+
+@app.route("/energy-per-capita/<country>")
+def energy_per_capita(country):
+    df = pd.read_csv("datasets/globalwarmingdata.csv")
+    result_df = df[df["country"] == country]
+    result_df = result_df[["year", "energy_per_capita"]]
+    result_df.fillna("N/A", inplace=True)
+    result_dict = dict(zip(result_df["year"], result_df["energy_per_capita"]))
+    return jsonify(result_dict)
 
 @app.route("/co2-growth-abs/<country>")
 def co2_growth_abs(country):
@@ -95,38 +97,76 @@ def co2_per_capita(country):
     result_dict = dict(zip(result_df["year"], result_df["co2_per_capita"]))
     return jsonify(result_dict)
 
-@app.route("/energy-per-capita/<country>")
-def energy_per_capita(country):
-    df = pd.read_csv("datasets/globalwarmingdata.csv")
-    result_df = df[df["country"] == country]
-    result_df = result_df[["year", "energy_per_capita"]]
-    result_df.fillna("N/A", inplace=True)
-    result_dict = dict(zip(result_df["year"], result_df["energy_per_capita"]))
-    return jsonify(result_dict)
+@app.route("/per-capita/<country>")
+def per_capita(country):
+    merged_df = init_global_data()
+    result_df = merged_df[merged_df["country"] == country]
+    per_capita_columns = [
+        col for col in result_df.columns
+        if "per_capita" in col
+    ]
 
-@app.route("/ghg-per-capita/<country>")
+    if 'year' in result_df.columns:
+        per_capita_columns.append('year')
+
+    per_capita_data_df = result_df[per_capita_columns].copy()
+    per_capita_data_df.fillna(0, inplace=True)
+    return jsonify(per_capita_data_df.to_dict(orient="records"))
+
+@app.route("/cumulative/<country>")
+def cumulative(country):
+    merged_df = init_global_data()
+    result_df = merged_df[merged_df["country"] == country]
+    cumulative_columns = [
+        col for col in result_df.columns
+        if "cumulative" in col and "share" not in col
+    ]
+
+    if 'year' in result_df.columns:
+        cumulative_columns.append('year')
+
+    cumulative_data_df = result_df[cumulative_columns].copy()
+    cumulative_data_df.fillna(0, inplace=True)
+    return jsonify(cumulative_data_df.to_dict(orient="records"))
+
+@app.route("/share-global/<country>")
+def share_global(country):
+    merged_df = init_global_data()
+    result_df = merged_df[merged_df["country"] == country]
+    share_columns = [
+        col for col in result_df.columns
+        if "share_global" in col
+    ]
+
+    if 'year' in result_df.columns:
+        share_columns.append('year')
+
+    share_data_df = result_df[share_columns].copy()
+    share_data_df.fillna(0, inplace=True)
+    return jsonify(share_data_df.to_dict(orient="records"))
+
+@app.route("/ghg/<country>")
 def ghg_per_capita(country):
     df = pd.read_csv("datasets/globalwarmingdata.csv")
     result_df = df[df["country"] == country]
-    result_df = result_df[["year", "ghg_per_capita", "ghg_excluding_lucf_per_capita"]]
-    result_df.fillna("N/A", inplace=True)
-    result_dict = {
-        row["year"]: {
-            "ghg_per_capita": row["ghg_per_capita"],
-            "ghg_excluding_lucf_per_capita": row["ghg_excluding_lucf_per_capita"],
-        }
-        for _, row in result_df.iterrows()
-    }
-    return jsonify(result_dict)
+
+    ghg_columns = [
+        col for col in result_df.columns
+        if "ghg" in col
+    ]
+
+    if 'year' in result_df.columns:
+        ghg_columns.append('year')
+
+    ghg_data_df = result_df[ghg_columns].copy()
+    ghg_data_df.fillna(0, inplace=True)
+
+    return jsonify(ghg_data_df.to_dict(orient="records"))
 
 @app.route("/gdp/<country>")
 def country_gdp(country):
-    # Load datasets
-    df1 = pd.read_csv("datasets/globalwarmingdata.csv")
-    df2 = filter_world_data()
-
     # Merge datasets on 'country'
-    merged_df = pd.merge(df1, df2, on="country")
+    merged_df = init_global_data()
 
     # Filter rows by country
     result_df = merged_df[merged_df["country"] == country]
@@ -150,32 +190,9 @@ def country_gdp(country):
     # Convert the filtered DataFrame to JSON format
     return jsonify(gdp_data_df.to_dict(orient="records"))
 
-@app.route("/energy-per-gdp/<country>")
-def energy_per_gdp(country):
-    df = pd.read_csv("datasets/globalwarmingdata.csv")
-    result_df = df[df["country"] == country]
-    result_df = result_df[["year", "energy_per_gdp"]]
-    result_df.fillna("N/A", inplace=True)
-    result_dict = dict(zip(result_df["year"], result_df["energy_per_gdp"]))
-    return jsonify(result_dict)
-
-@app.route("/co2-per-energy/<country>")
-def co2_per_energy(country):
-    df = pd.read_csv("datasets/globalwarmingdata.csv")
-    result_df = df[df["country"] == country]
-    result_df = result_df[["year", "co2_per_unit_energy"]]
-    result_df.fillna("N/A", inplace=True)
-    result_dict = dict(zip(result_df["year"], result_df["co2_per_unit_energy"]))
-    return jsonify(result_dict)
-
 @app.route("/co2/<country>")
 def country_co2(country):
-    # Load datasets
-    df1 = pd.read_csv("datasets/globalwarmingdata.csv")
-    df2 = filter_world_data()
-
-    # Merge datasets on 'country'
-    merged_df = pd.merge(df1, df2, on="country")
+    merged_df = init_global_data()
 
     # Filter rows by country
     result_df = merged_df[merged_df["country"] == country]
