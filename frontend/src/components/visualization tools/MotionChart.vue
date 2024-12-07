4f290c233327b9ced4265b5de8c12ece6af4a9ba
HSK <haakonsk@live.no>
2024-12-07 19:24:55 +0100

    Made labels white

--- frontend/src/components/visualization tools/MotionChart.vue
+++ frontend/src/components/visualization tools/MotionChart.vue
@@ -120,233 +120,252 @@ export default {
       this.yearLabel.set("text", year.toString());
     }
   },
-  async initializeChart() {
-    let root = am5.Root.new("chartdiv_MotionChart");
+   async initializeChart() {
+     let root = am5.Root.new("chartdiv_MotionChart");
 
-    root.setThemes([am5themes_Animated.new(root)]);
+     root.setThemes([am5themes_Animated.new(root)]);
 
-    this.generateYearData();
+     this.generateYearData();
 
-    let mainContainer = root.container.children.push(am5.Container.new(root, {
-      width: am5.p100,
-      height: am5.p100,
-      layout: root.verticalLayout
-    }));
+     let mainContainer = root.container.children.push(am5.Container.new(root, {
+       width: am5.p100,
+       height: am5.p100,
+       layout: root.verticalLayout
+     }));
 
-    let chart = mainContainer.children.push(am5xy.XYChart.new(root, {
-      panX: true,
-      panY: true,
-      wheelY: "zoomXY",
-      pinchZoomX: true,
-      pinchZoomY: true
-    }));
+     let chart = mainContainer.children.push(am5xy.XYChart.new(root, {
+       panX: true,
+       panY: true,
+       wheelY: "zoomXY",
+       pinchZoomX: true,
+       pinchZoomY: true
+     }));
 
-    let xAxis = chart.xAxes.push(am5xy.ValueAxis.new(root, {
-      min: 0,
-      max: 6200,
-      renderer: am5xy.AxisRendererX.new(root, { minGridDistance: 50 }),
-      tooltip: am5.Tooltip.new(root, {})
-    }));
+     let xAxis = chart.xAxes.push(am5xy.ValueAxis.new(root, {
+       min: 0,
+       max: 6200,
+       renderer: am5xy.AxisRendererX.new(root, { minGridDistance: 50 }),
+       tooltip: am5.Tooltip.new(root, {})
+     }));
 
-    xAxis.children.push(am5.Label.new(root, { text: "Co2 Emissions", x: am5.p50, centerX: am5.p50 }));
+     xAxis.children.push(am5.Label.new(root, { text: "Co2 Emissions", x: am5.p50, centerX: am5.p50, fill: am5.color(0xffffff) }));
 
-    let yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
-      min: 0,
-      max: 100,
-      renderer: am5xy.AxisRendererY.new(root, {}),
-      tooltip: am5.Tooltip.new(root, {})
-    }));
+     xAxis.get("renderer").labels.template.setAll({
+       fill: am5.color(0xffffff)
+     });
 
-    yAxis.children.moveValue(am5.Label.new(root, {
-      text: "GDP",
-      rotation: -90,
-      y: am5.p50,
-      centerX: am5.p50
-    }), 0);
+     let yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+       min: 0,
+       max: 100,
+       renderer: am5xy.AxisRendererY.new(root, {}),
+       tooltip: am5.Tooltip.new(root, {})
+     }));
 
-    this.bubbleSeries = chart.series.push(am5xy.LineSeries.new(root, {
-      calculateAggregates: true,
-      xAxis: xAxis,
-      yAxis: yAxis,
-      valueYField: "y",
-      valueXField: "x",
-      valueField: "value"
-    }));
+     yAxis.children.moveValue(am5.Label.new(root, {
+       text: "GDP",
+       rotation: -90,
+       y: am5.p50,
+       centerX: am5.p50,
+       fill: am5.color(0xffffff)
+     }), 0);
 
-    this.bubbleSeries.strokes.template.set("visible", false);
+     yAxis.get("renderer").labels.template.setAll({
+       fill: am5.color(0xffffff)
+     });
 
-    let circleTemplate = am5.Template.new({ tooltipY: 0 });
-    circleTemplate.states.create("transparent", { opacity: 0.15 });
+     this.bubbleSeries = chart.series.push(am5xy.LineSeries.new(root, {
+       calculateAggregates: true,
+       xAxis: xAxis,
+       yAxis: yAxis,
+       valueYField: "y",
+       valueXField: "x",
+       valueField: "value"
+     }));
 
-    circleTemplate.events.on("pointerover", this.handleOver);
-    circleTemplate.events.on("pointerout", this.handleOut);
-    circleTemplate.events.on("click", this.handleClick);
+     this.bubbleSeries.strokes.template.set("visible", false);
 
-    this.bubbleSeries.bullets.push(() => {
-      let bulletCircle = am5.Circle.new(root, {
-        radius: 5,
-        templateField: "settings",
-        fillOpacity: 0.9,
-        tooltipText: "[fontSize:18px; bold]{name}[/]\nCo2 emissions: {valueY} tonnes per person\nCo2 emissions: {valueX} million tonnes",
-      }, circleTemplate);
-      return am5.Bullet.new(root, {
-        sprite: bulletCircle
-      });
-    });
+     let circleTemplate = am5.Template.new({ tooltipY: 0 });
+     circleTemplate.states.create("transparent", { opacity: 0.15 });
 
-    this.bubbleSeries.set("heatRules", [{
-      target: circleTemplate,
-      min: 3,
-      max: 6,
-      dataField: "value",
-      key: "radius", maxValue: 10
-    }]);
+     circleTemplate.events.on("pointerover", this.handleOver);
+     circleTemplate.events.on("pointerout", this.handleOut);
+     circleTemplate.events.on("click", this.handleClick);
 
-    let lineSeries = chart.series.push(am5xy.LineSeries.new(root, {
-      valueXField: "x",
-      valueYField: "y",
-      xAxis: xAxis,
-      yAxis: yAxis,
-      stroke: am5.color(0x00000)
-    }));
+     this.bubbleSeries.bullets.push(() => {
+       let bulletCircle = am5.Circle.new(root, {
+         radius: 5,
+         templateField: "settings",
+         fillOpacity: 0.9,
+         tooltipText: "[fontSize:18px; bold]{name}[/]\nCo2 emissions: {valueY} tonnes per person\nCo2 emissions: {valueX} million tonnes",
+       }, circleTemplate);
+       return am5.Bullet.new(root, {
+         sprite: bulletCircle
+       });
+     });
 
-    lineSeries.strokes.template.set("strokeOpacity", 0.3);
+     this.bubbleSeries.set("heatRules", [{
+       target: circleTemplate,
+       min: 3,
+       max: 6,
+       dataField: "value",
+       key: "radius", maxValue: 10
+     }]);
 
-    lineSeries.bullets.push(() => {
-      let bulletCircle = am5.Circle.new(root, {
-        radius: 2,
-        fill: lineSeries.stroke
-      });
-      return am5.Bullet.new(root, {
-        sprite: bulletCircle
-      });
-    });
+     let lineSeries = chart.series.push(am5xy.LineSeries.new(root, {
+       valueXField: "x",
+       valueYField: "y",
+       xAxis: xAxis,
+       yAxis: yAxis,
+       stroke: am5.color(0x00000)
+     }));
 
-    chart.set("cursor", am5xy.XYCursor.new(root, {
-      xAxis: xAxis,
-      yAxis: yAxis,
-      snapToSeries: [this.bubbleSeries]
-    }));
+     lineSeries.strokes.template.set("strokeOpacity", 0.3);
 
-    chart.set("scrollbarX", am5.Scrollbar.new(root, {
-      orientation: "horizontal",
-      exportable: false
-    }));
+     lineSeries.bullets.push(() => {
+       let bulletCircle = am5.Circle.new(root, {
+         radius: 2,
+         fill: lineSeries.stroke
+       });
+       return am5.Bullet.new(root, {
+         sprite: bulletCircle
+       });
+     });
 
-    chart.set("scrollbarY", am5.Scrollbar.new(root, {
-      orientation: "vertical",
-      exportable: false
-    }));
+     chart.set("cursor", am5xy.XYCursor.new(root, {
+       xAxis: xAxis,
+       yAxis: yAxis,
+       snapToSeries: [this.bubbleSeries]
+     }));
+
+     chart.set("scrollbarX", am5.Scrollbar.new(root, {
+       orientation: "horizontal",
+       exportable: false
+     }));
+
+     chart.set("scrollbarY", am5.Scrollbar.new(root, {
+       orientation: "vertical",
+       exportable: false
+     }));
 
      this.yearLabel = chart.plotContainer.children.push(am5.Label.new(root, {
-      text: this.currentYear.toString(),
-      fontSize: "10em",
-      fill: am5.color(0x000000),
-      opacity: 0.15,
-      x: am5.p50,
-      y: am5.p50,
-      fontFamily: "Courier New",
-      textAlign: "right",
-      centerY: am5.p50,
-      centerX: am5.p50
-    }));
+       text: this.currentYear.toString(),
+       fontSize: "10em",
+       fill: am5.color(0xffffff),
+       opacity: 0.15,
+       x: am5.p50,
+       y: am5.p50,
+       fontFamily: "Courier New",
+       textAlign: "right",
+       centerY: am5.p50,
+       centerX: am5.p50
+     }));
 
-    let yearSliderContainer = mainContainer.children.push(am5.Container.new(root, {
-      width: am5.percent(100),
-      layout: root.horizontalLayout,
-      paddingLeft: 70,
-      paddingRight: 40,
-      exportable: false
-    }));
+     let yearSliderContainer = mainContainer.children.push(am5.Container.new(root, {
+       width: am5.percent(100),
+       layout: root.horizontalLayout,
+       paddingLeft: 70,
+       paddingRight: 40,
+       exportable: false
+     }));
 
-    let playButton = yearSliderContainer.children.push(am5.Button.new(root, {
-      themeTags: ["play"],
-      centerY: am5.p50,
-      marginRight: 20,
-      icon: am5.Graphics.new(root, {
-        themeTags: ["icon"]
-      })
-    }));
+     let playButton = yearSliderContainer.children.push(am5.Button.new(root, {
+       themeTags: ["play"],
+       centerY: am5.p50,
+       marginRight: 20,
+       icon: am5.Graphics.new(root, {
+         themeTags: ["icon"]
+       })
+     }));
 
-    playButton.events.on("click", () => {
-      if (playButton.get("active")) {
-        slider.set("start", slider.get("start") + 0.0001);
-      } else {
-        slider.animate({
-          key: "start",
-          to: 1,
-          duration: 30000 * (1 - slider.get("start"))
-        });
-      }
-    });
+     playButton.events.on("click", () => {
+       if (playButton.get("active")) {
+         slider.set("start", slider.get("start") + 0.0001);
+       } else {
+         slider.animate({
+           key: "start",
+           to: 1,
+           duration: 30000 * (1 - slider.get("start"))
+         });
+       }
+     });
 
-    let slider = yearSliderContainer.children.push(am5.Slider.new(root, {
-      orientation: "horizontal",
-      start: 0,
-      centerY: am5.p50
-    }));
+     let slider = yearSliderContainer.children.push(am5.Slider.new(root, {
+       orientation: "horizontal",
+       start: 0,
+       centerY: am5.p50
+     }));
 
-    slider.on("start", (start) => {
-      if (start === 1) {
-        playButton.set("active", false);
-      }
-    });
+     slider.on("start", (start) => {
+       if (start === 1) {
+         playButton.set("active", false);
+       }
+     });
 
-    slider.events.on("rangechanged", () => {
-      this.updateSeriesData(
-        this.firstYear + Math.round(slider.get("start", 0) * (this.lastYear - this.firstYear))
-      );
-    });
+     slider.events.on("rangechanged", () => {
+       this.updateSeriesData(
+           this.firstYear + Math.round(slider.get("start", 0) * (this.lastYear - this.firstYear))
+       );
+     });
 
-    let navMap = chart.plotContainer.children.push(am5map.MapChart.new(root, {
-      projection: am5map.geoNaturalEarth1(),
-      rotationX: -11,
-      width: 250,
-      height: 150,
-      x: am5.percent(100),
-      y: am5.percent(100),
-      centerY: am5.percent(100),
-      centerX: am5.percent(100),
-      panY: "none",
-      panX: "none"
-    }));
+     let navMap = chart.plotContainer.children.push(am5map.MapChart.new(root, {
+       projection: am5map.geoNaturalEarth1(),
+       rotationX: -11,
+       width: 250,
+       height: 150,
+       x: am5.percent(100),
+       y: am5.percent(100),
+       centerY: am5.percent(100),
+       centerX: am5.percent(100),
+       panY: "none",
+       panX: "none"
+     }));
 
      this.polygonSeries = navMap.series.push(am5map.MapPolygonSeries.new(root, {
-      geoJSON: am4geodata_continentsLow,
-      exclude: ["antarctica"]
-    }));
+       geoJSON: am4geodata_continentsLow,
+       exclude: ["antarctica"]
+     }));
 
-    let polygonTemplate = this.polygonSeries.mapPolygons.template;
+     let polygonTemplate = this.polygonSeries.mapPolygons.template;
 
-    polygonTemplate.setAll({
-      templateField: "settings",
-      tooltipText: "{name}",
-      interactive: true
-    });
+     polygonTemplate.setAll({
+       templateField: "settings",
+       tooltipText: "{name}",
+       interactive: true
+     });
 
-    polygonTemplate.states.create("disabled", {
-      fill: root.interfaceColors.get("disabled")
-    });
+     polygonTemplate.states.create("disabled", {
+       fill: root.interfaceColors.get("disabled")
+     });
 
-    polygonTemplate.events.on("pointerover", this.handleContinentOver);
-    polygonTemplate.events.on("click", this.handleContinentClick);
-    polygonTemplate.events.on("pointerout", this.handleOut);
+     polygonTemplate.events.on("pointerover", this.handleContinentOver);
+     polygonTemplate.events.on("click", this.handleContinentClick);
+     polygonTemplate.events.on("pointerout", this.handleOut);
 
-    this.polygonSeries.data.setAll([
-      { id: "europe", code: "EU", settings: { fill: this.colors.EU } },
-      { id: "northAmerica", code: "NA", settings: { fill: this.colors.NA } },
-      { id: "southAmerica", code: "SA", settings: { fill: this.colors.SA } },
-      { id: "asia", code: "AS", settings: { fill: this.colors.AS } },
-      { id: "africa", code: "AF", settings: { fill: this.colors.AF } },
-      { id: "oceania", code: "OC", settings: { fill: this.colors.OC } }
-    ]);
+     this.polygonSeries.data.setAll([
+       { id: "europe", code: "EU", settings: { fill: this.colors.EU } },
+       { id: "northAmerica", code: "NA", settings: { fill: this.colors.NA } },
+       { id: "southAmerica", code: "SA", settings: { fill: this.colors.SA } },
+       { id: "asia", code: "AS", settings: { fill: this.colors.AS } },
+       { id: "africa", code: "AF", settings: { fill: this.colors.AF } },
+       { id: "oceania", code: "OC", settings: { fill: this.colors.OC } }
+     ]);
 
-    await this.updateSeriesData(this.currentYear);
-    const rawData = toRaw(this.yearData[this.currentYear]);
-    this.bubbleSeries.data.setAll(rawData);
-    this.bubbleSeries.appear(1000);
-    chart.appear(1000, 100);
-  },
+     // Create and configure the legend
+     let legend = chart.children.push(am5.Legend.new(root, {
+       centerX: am5.p50,
+       x: am5.p50
+     }));
+
+     legend.labels.template.setAll({
+       fill: am5.color(0xffffff)
+     });
+
+     await this.updateSeriesData(this.currentYear);
+     const rawData = toRaw(this.yearData[this.currentYear]);
+     this.bubbleSeries.data.setAll(rawData);
+     this.bubbleSeries.appear(1000);
+     chart.appear(1000, 100);
+   },
   handleOver(e) {
     let target = e.target;
     am5.array.each(this.bubbleSeries.dataItems, (dataItem) => {
