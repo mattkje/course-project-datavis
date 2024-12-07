ee51e60201d9e18e89e9b6a564fa667287fcea56
HSK <haakonsk@live.no>
2024-12-07 19:54:13 +0100

    Added some images

--- frontend/src/components/visualization tools/InformationalBox.vue
+++ frontend/src/components/visualization tools/InformationalBox.vue
@@ -16,24 +16,24 @@ const tabs = [
     id: 2,
     name: 'Middle East - Oil Rush',
     content: 'The discovery of oil in the Middle East in the early 20th century transformed the region into a global economic powerhouse. Countries like Saudi Arabia, Iran, and Iraq quickly became major players in the global energy market, as their vast oil reserves were tapped to meet the world’s growing demand for fuel. This resource brought immense wealth, reshaping the region’s economies and elevating the political influence of oil-rich nations. Western powers and multinational corporations heavily invested in Middle Eastern oil, further accelerating its extraction and export. The oil boom not only enriched the region but also cemented its central role in global energy and politics.',
-    largeImageURL: '',
-    smallImageURL1: '',
+    largeImageURL: 'https://cdn.punchng.com/wp-content/uploads/2023/06/14111510/01-gas-1.jpg',
+    smallImageURL1: 'https://www.arabnews.com/sites/default/files/styles/n_670_395/public/2017/03/07/867641-1586784408.jpg?itok=BUm_0SVy',
     smallImageURL2: 'https://upload.wikimedia.org/wikipedia/commons/c/c4/Dammam_No._7_on_March_4%2C_1938.jpg',
   },
   {
     id: 3,
     name: 'USA - Co2 Domination',
     content: 'The United States became an emissions superpower through decades of industrialization, urban growth, and heavy reliance on fossil fuels. Starting in the late 19th century, industries like steel, manufacturing, and automotive production, powered by coal, oil, and gas, drove massive carbon emissions. The post-World War II economic boom and suburbanization further increased energy use, with car culture playing a major role. As one of the world’s largest economies, the U.S. has long been a top emitter, with high per capita energy consumption and a focus on economic growth. Despite progress in renewables, its industrial base and transportation systems remain major sources of emissions.',
-    largeImageURL: '',
-    smallImageURL1: '',
-    smallImageURL2: '',
+    largeImageURL: 'https://imageio.forbes.com/blogs-images/jamesconca/files/2015/04/PaloVerde-nuclear.jpg?format=jpg&height=600&width=1200&fit=bounds',
+    smallImageURL1: 'https://i.insider.com/629faf1b7498590019651323?width=700',
+    smallImageURL2: 'https://sitescdn.wearevennture.co.uk/public/nes-fircroft/mediahub/austrian-national-library-exgfrvfjk4m-unsplash-910332978e534cfe9c3028b2dc1087ac.jpg',
   },
   {
     id: 4, name: 'China - The Worlds Factory', content: '\n' +
         'China became the world’s factory as global businesses capitalized on its low labor costs, abundant workforce, and favorable manufacturing policies. Starting in the late 20th century, foreign companies outsourced production to China, taking advantage of its rapidly developing infrastructure and government incentives. This shift allowed multinational corporations to reduce costs while accessing China’s massive export capabilities. Over time, China built a vast industrial base, producing everything from electronics to textiles at unprecedented scales. The global demand for cheap goods fueled this growth. However, this economic model also led to significant environmental and social challenges, including high carbon emissions and labor issues.',
-    largeImageURL: '',
-    smallImageURL1: '',
-    smallImageURL2: '',
+    largeImageURL: 'https://image.cnbcfm.com/api/v1/image/104859991-Foxconn_factory_.jpg?v=1710508838&w=1920&h=1080',
+    smallImageURL1: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9hxpUGBGsk1JgH1kvCvWbEp5DTDr1xbt28g&s',
+    smallImageURL2: 'https://chinatownatlas.org/wp-content/uploads/2014/06/Garment-Factory-1024x794.jpg',
   },
 ];
 
@@ -63,17 +63,17 @@ defineExpose({selectTab});
       </div>
       <div class="image-gallery">
         <div
-            v-if="tabs.find(tab => tab.id === selectedTab)?.largeImageURL"
+            v-if="tabs.filter(tab => tab.id === selectedTab)[0]?.largeImageURL"
             class="main-image"
             :style="{ backgroundImage: 'url(' + tabs.find(tab => tab.id === selectedTab).largeImageURL + ')' }">
         </div>
         <div
-            v-if="tabs.find(tab => tab.id === selectedTab)?.smallImageURL1"
+            v-if="tabs.filter(tab => tab.id === selectedTab)[0]?.smallImageURL1"
             class="sub-image"
-            :style="{ backgroundImage: 'url(' + tabs.find(tab => tab.id === selectedTab).smallImageURL1 + ')' }">
+            :style="{ backgroundImage: 'url(' + tabs.filter(tab => tab.id === selectedTab)[0].smallImageURL1 + ')' }">
         </div>
         <div
-            v-if="tabs.find(tab => tab.id === selectedTab)?.smallImageURL2"
+            v-if="tabs.filter(tab => tab.id === selectedTab)[0]?.smallImageURL2"
             class="sub-image"
             :style="{ backgroundImage: 'url(' + tabs.find(tab => tab.id === selectedTab).smallImageURL2 + ')' }">
         </div>
@@ -201,6 +201,8 @@ h3 {
   justify-content: center;
   color: white;
   font-weight: bold;
+  background-size: cover;
+  background-position: center;
 }
 
 .sub-image {
@@ -223,12 +225,13 @@ h3 {
   .content {
     flex-direction: column;
     gap: 20px;
-    height: auto;
+    height: 100%;
   }
 
   .image-gallery {
-    grid-template-columns: 1fr;
-    grid-template-rows: 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr;
+    min-height: 380px;
   }
 
   .main-image {
@@ -237,9 +240,13 @@ h3 {
   }
 
   .sub-image {
-    grid-column: 1 / 2;
-    grid-row: 2 / 3;
+    grid-column: 2 / 3;
+    grid-row: 1 / 2;
   }
 
+  .sub-image:nth-child(3) {
+    grid-column: 3 / 4;
+    grid-row: 1 / 2;
+  }
 }
 </style>
